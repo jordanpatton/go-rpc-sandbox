0775be8b4c421a5fa9590dc7fe8c2300a841d728
Jordan Patton <jordanpatton@gmail.com>
2020-01-29 18:14:44 -0800

    more operator cases

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
     - `curl http://localhost:4002/calculate/4/+/2`.
     - `curl http://localhost:4002/calculate/4/-/2`.
     - `curl http://localhost:4002/calculate/4/*/2`.
-    - Not sure how to divide yet. (`%2F` is not accepted as `/`.)
+    - `curl http://localhost:4002/calculate/4/divide/2`.
 
 # Links
 - Building an Basic API with gRPC and Protobuf by Tensor Programming ([video](https://www.youtube.com/watch?v=Y92WWaZJl24), [repo](https://github.com/tensor-programming/grpc_tutorial))
--- server/main.go
+++ server/main.go
@@ -14,13 +14,13 @@ type server struct{}
 func (s *server) Calculate(ctx context.Context, req *proto.CalculateRequest) (*proto.CalculateResponse, error) {
 	operator, x, y := req.GetInputs().GetOperator(), req.GetInputs().GetX(), req.GetInputs().GetY()
 	switch operator {
-	case "+":
+	case "+", "add", "plus":
 		return &proto.CalculateResponse{Output: x + y}, nil
-	case "-":
+	case "-", "subtract", "minus":
 		return &proto.CalculateResponse{Output: x - y}, nil
-	case "*":
+	case "*", "multiply", "times":
 		return &proto.CalculateResponse{Output: x * y}, nil
-	case "/":
+	case "/", "divide", "divided by":
 		return &proto.CalculateResponse{Output: x / y}, nil
 	default:
 		return &proto.CalculateResponse{Output: -1}, nil
